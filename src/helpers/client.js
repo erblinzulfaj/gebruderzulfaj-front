76e2c2de9867dc77daa6bcd1e4548543739cbabe
Erblim Zulfaj <erblin.zulfaj@student.cacttus.educatin>
2024-12-11 18:22:13 -0500

    changes

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -1,23 +1,24 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080',
-    headers: {'Content-Type': 'application/json'}
+  baseURL: "https://gebruderzulfaj-47d204770105.herokuapp.com",
+  headers: { "Content-Type": "application/json" },
 });
 
-
 // Add a request interceptor
-instance.interceptors.request.use(function (config) {
+instance.interceptors.request.use(
+  function (config) {
     // Do something before request is sent
 
     const token = localStorage.getItem("token");
     config.headers.Authorization = token ? `Bearer ${token}` : null;
     // console.log(config.headers.Authorization)
 
-
     return config;
-}, function (error) {
+  },
+  function (error) {
     // Do something with request error
     return Promise.reject(error);
-});
+  }
+);
 export default instance;
