d064bbe95a9c1c3488545a879728bef72900d5de
Erblim Zulfaj <erblin.zulfaj@student.cacttus.educatin>
2024-12-17 13:47:57 -0500

    updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from  "vue-router"
+import { createRouter, createWebHistory } from "vue-router";
 import Login from "@/components/crud-stock/Login.vue";
 import HomeView from "@/views/HomeView.vue";
 import AddProduct from "@/components/crud-stock/AddProduct.vue";
@@ -13,60 +13,59 @@ import FileReader from "@/components/crud-stock/FileReader.vue";
 import UpdateQuantity from "@/components/crud-stock/UpdateQuantity.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            name:'login',
-            component: Login
-        },
-        {
-            path: '/home',
-            name:'home',
-            component: GetProductsCategories
-        },
-        {
-            path: '/products',
-            name:'Products',
-            component: GetProductsByCategory
-        },
-        {
-            path: '/less-quantity',
-            name: 'less-quantity',
-            component: LessQuantity
-        },
-        {
-            path: '/add',
-            name:'add-product',
-            component: AddProduct
-        },
-        {
-            path: '/discount-history',
-            name:'discount',
-            component: DecrementHistory
-        },
-        {
-            path: '/increment-history',
-            name:'increment-history',
-            component: IncrementHistory
-        },
-        {
-            path: '/update/:id',
-            name:'update',
-            component: ModifyProduct
-        },
-        {
-            path: '/file-reader',
-            name:'file-reader',
-            component: FileReader
-        },
-        {
-            path: '/update-quantity',
-            name:'update-quantity',
-            component: UpdateQuantity
-        }
-
-    ]
-})
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "https://www.xn--stock-gebrderzulfaj-fbc.com/ ",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "https://www.xn--stock-gebrderzulfaj-fbc.com/home",
+      name: "home",
+      component: GetProductsCategories,
+    },
+    {
+      path: "https://www.xn--stock-gebrderzulfaj-fbc.com/products",
+      name: "Products",
+      component: GetProductsByCategory,
+    },
+    {
+      path: "https://www.xn--stock-gebrderzulfaj-fbc.com/less-quantity",
+      name: "less-quantity",
+      component: LessQuantity,
+    },
+    {
+      path: "https://www.xn--stock-gebrderzulfaj-fbc.com/add",
+      name: "add-product",
+      component: AddProduct,
+    },
+    {
+      path: "https://www.xn--stock-gebrderzulfaj-fbc.com/discount-history",
+      name: "discount",
+      component: DecrementHistory,
+    },
+    {
+      path: "https://www.xn--stock-gebrderzulfaj-fbc.com/increment-history",
+      name: "increment-history",
+      component: IncrementHistory,
+    },
+    {
+      path: "https://www.xn--stock-gebrderzulfaj-fbc.com/update/:id",
+      name: "update",
+      component: ModifyProduct,
+    },
+    {
+      path: "https://www.xn--stock-gebrderzulfaj-fbc.com/file-reader",
+      name: "file-reader",
+      component: FileReader,
+    },
+    {
+      path: "https://www.xn--stock-gebrderzulfaj-fbc.com/update-quantity",
+      name: "update-quantity",
+      component: UpdateQuantity,
+    },
+  ],
+});
 
 export default router;
